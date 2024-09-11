666d2d408a9faee34ca7dd38de55d20b0a9c19a1
Catalyst <Catalyst>
2024-09-11 13:02:39 +0100

    Add NotFound file.

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center lg:text-[3rem] text-[2rem]">
+      Page Not Found (404 error)
+    </div>
+  );
+}
+
+export default NotFound;
